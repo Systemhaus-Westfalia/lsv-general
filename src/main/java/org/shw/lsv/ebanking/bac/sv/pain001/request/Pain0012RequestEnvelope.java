63c34339f379265ef5bfbc1b46f7c6079e0a34b9
marcalwestf <mariocalderon007@gmail.com>
2025-05-23 02:05:42 -0600

    Small correction

--- src/main/java/org/shw/lsv/ebanking/bac/sv/pain001/request/Pain0012RequestEnvelope.java
+++ src/main/java/org/shw/lsv/ebanking/bac/sv/pain001/request/Pain0012RequestEnvelope.java
@@ -1,11 +1,11 @@
 package org.shw.lsv.ebanking.bac.sv.pain001.request;
 
 import org.shw.lsv.ebanking.bac.sv.handling.JsonValidationExceptionCollector;
-import org.shw.lsv.ebanking.bac.sv.handling.Pain001RequestParams;
+import org.shw.lsv.ebanking.bac.sv.handling.PAIN001RequestParams;
 
 public class Pain0012RequestEnvelope {
 
-    public Pain0012RequestEnvelope(Pain001RequestParams params, JsonValidationExceptionCollector collector) {
+    public Pain0012RequestEnvelope(PAIN001RequestParams params, JsonValidationExceptionCollector collector) {
         //TODO Auto-generated constructor stub
     }
 
