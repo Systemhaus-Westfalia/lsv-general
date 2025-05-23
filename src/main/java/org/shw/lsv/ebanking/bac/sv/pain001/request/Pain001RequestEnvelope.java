493e91f41ea7a329fb957f27569fca7f0da795ea
marcalwestf <mariocalderon007@gmail.com>
2025-05-23 10:42:41 -0600

    Klasse vergessen

---
+++ src/main/java/org/shw/lsv/ebanking/bac/sv/pain001/request/Pain001RequestEnvelope.java
@@ -0,0 +1,12 @@
+package org.shw.lsv.ebanking.bac.sv.pain001.request;
+
+import org.shw.lsv.ebanking.bac.sv.handling.JsonValidationExceptionCollector;
+import org.shw.lsv.ebanking.bac.sv.handling.RequestParams;
+
+public class Pain001RequestEnvelope {
+
+    public Pain001RequestEnvelope(RequestParams params, JsonValidationExceptionCollector collector) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
